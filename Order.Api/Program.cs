fc0b2cac0f3d6ee90211b34c675e46e7daa53602
gizemorak <gizemorak@etimaden.gov.tr>
2026-02-23 11:54:34 +0300

    program.cs sadeleştirildi.

--- Order.Api/Program.cs
+++ Order.Api/Program.cs
@@ -1,14 +1,91 @@
 using Bus.Shared.Options;
-using Order.Api.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using OrderApplication.Orders.Commands.CancelOrder;
+using OrderApplication.Orders.Commands.CreateOrder;
+using OrderApplication.Orders.Queries.GetOrder;
+using OrderDomain.Repositories;
+using OrderPersistence;
+using OrderPersistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// register services via extension
-builder.Services.AddApiServices(builder.Configuration);
+            // Add services to the container.
+            builder.Services.AddAuthorization();
 
-var app = builder.Build();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-// configure middleware and endpoints via extension
-app.UseApi();
+            builder.Services.Configure<ServiceBusOption>(
+    builder.Configuration.GetSection(nameof(ServiceBusOption)));
+
+            builder.Services.AddSingleton<ServiceBusOption>(sp =>
+            {
+                IOptions<ServiceBusOption> optionsServiceBus = sp.GetRequiredService<IOptions<ServiceBusOption>>();
+                return optionsServiceBus.Value;
+
+            });
+
+            string connectionString = builder.Configuration.GetConnectionString("Database");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(
+            (sp, optionsBuilder) =>
+            {
+
+                optionsBuilder.UseSqlServer(connectionString);
+           
+            });
+
+
+            builder.Services.AddMediatR(cfg =>
+      cfg.RegisterServicesFromAssemblyContaining<GetByIdOrderCommandHandler>());
+
+
+
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddSingleton<IBusService, RabbitMqBusService>(sp =>
+            {
+                ServiceBusOption serviceBusOptions = sp.GetRequiredService<ServiceBusOption>();
+
+                RabbitMqBusService rabbitMqBus = new RabbitMqBusService(serviceBusOptions);
+                rabbitMqBus.Init().Wait();
+                rabbitMqBus.CreateExchanges().Wait();
+                return rabbitMqBus;
+            });
+
+        
+            var app = builder.Build();
+
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+
+            app.MapPost("/sendorder", async (CreateOrderCommand request,IMediator mediator) =>
+            {
+
+                await mediator.Send(request);
+
+                return Results.Ok();
+               
+            })
+            .WithName("SendOrder")
+            .WithOpenApi();
 
 app.Run();
